02b11829c6d9fad6da4d734c0d71187d8122f74f
Jan Kranzen <jan.kranzen@cegeka.com>
2025-02-26 09:51:14 +0000

    Enhance security workflows and remove trailing whitespace from AWS secret key

--- terraform/main.tf
+++ terraform/main.tf
@@ -19,7 +19,7 @@ terraform {
 provider "aws" {
   region     = "us-west-2"
   access_key = "AKIAIOSFODNN7EXAMPLE"
-  secret_key = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY sdfs"
+  secret_key = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
 }
 
 resource "aws_instance" "app_server" {
